e1eeb8970ee406d8ae41abcb60023e07f982ab89
Yuval Maron <yuvalmaron222@gmail.com>
2024-08-28 11:14:26 +0300

    Added questions

---
+++ questions.py
@@ -0,0 +1,38 @@
+
+
+
+def sum_to_hundred():
+    """
+    Write a function sum_to_hundred that returns the sum of all the numbers from 1 to 100 without using loops.
+    """
+
+
+def MostRepeated(str):
+    '''
+    Write a program that accepts a string str1 as input, and print the most repeated
+    character in it, for example: str1="littttttle"
+    the program would print "t"
+    '''
+
+
+def youngest_sibling(dict1):
+    '''
+    Write a function called youngest_sibling that takes in a dictionary of family names and ages.
+     Return the name of the youngest family member.
+    '''
+
+
+def tic_tac_toe(lst1):
+    '''
+    Write a function called tic_tac_toe that takes in a 3x3 2D list that represents a completed game of Tic Tac Toe.
+    Return "X" if "X" is the winner, return "O" if "O" is the winner.
+    If there is no winner, return False.
+    '''
+
+
+def neighbor_multiplication(lst):
+    '''
+    The following method, which is called neighbor_multiplication, takes in a list of integers,
+     they could be positive, negative, or zeros.
+    Find which pair of neighbor elements in this array have the largest value when multiplied.
+    '''
